882ea2f08d7c7993aa785e904aca5f6adad3d8f9
Gamal Osama <gamal5osama66@gmail.com>
2026-04-28 04:29:53 +0300

    add func to generate random string will use it in the data controller

--- source/controllers/BaseController.py
+++ source/controllers/BaseController.py
@@ -1,5 +1,7 @@
 from helpers.config import get_settings, Settings
 import os
+import random
+import string
 
 class BaseController:
     
@@ -9,3 +11,5 @@ class BaseController:
         self.base_dir = os.path.dirname(os.path.dirname(__file__)) # the source directory
         self.files_dir = os.path.join(self.base_dir, 'assets', 'files') 
 
+    def generate_random_string(self, length: int=12):
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
