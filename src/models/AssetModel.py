693514a4531e454c5e5d48eb0bd3be817720b282
Gamal Osama <gamal5osama66@gmail.com>
2026-05-02 00:28:26 +0300

    fix the old bug of .id and ._id

--- src/models/AssetModel.py
+++ src/models/AssetModel.py
@@ -40,7 +40,7 @@ class AssetModel(BaseDataModel):
 
     async def create_asset(self, asset: Asset):
         result = await self.collection.insert_one(asset.dict(by_alias=True, exclude_unset=True))
-        asset._id = result.inserted_id
+        asset.id = result.inserted_id
 
         return asset
     
