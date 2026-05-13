d7f9b6a4d8689721e04a301ee2566368e79c07e8
Gamal Osama <gamal5osama66@gmail.com>
2026-05-13 21:20:17 +0300

    adding the needed enums for pgvector support

--- src/stores/vectordb/VectoDBEnums.py
+++ src/stores/vectordb/VectoDBEnums.py
@@ -3,9 +3,28 @@ from enum import Enum
 
 class VectorDBEnums(Enum):
     QDRANT = "QDRANT"
+    PGVECTOR = "PGVECTOR"
 
 
 class DistanceMethodEnums(Enum):
     COSINE = "cosine"
     DOT = "dot"
     
+
+class PgVectorTableScemaEnums(Enum):
+    ID = "id"
+    TEXT = "text"
+    VECTOR = "vector"
+    CHUNK_ID = "chunk_id"
+    METADATA = "metadata"
+    _PREFIX = "pgvector_"
+
+
+class PgVectorDistanceMethodEnums(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_l2_ops"
+
+
+class PgVectorIndexTypeEnums(Enum):
+    IVFFLAT = "ivfflat"
+    HNSW = "hnsw"
