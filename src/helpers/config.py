f5e28600858c1c33d3f2e6f2acec1a84a84dffd4
Gamal Osama <gamal5osama66@gmail.com>
2026-06-18 10:18:02 +0300

    add the Celery task queue configuration to the settings and environment files

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -43,6 +43,12 @@ class Settings(BaseSettings):
     PRIMARY_LANGUAGE: str="english"
     DEFAULT_LANGUAGE: str="english"
 
+    CELERY_BROKER_URL: str = None
+    CELERY_RESULT_BACKEND: str = None
+    CELERY_TASK_SERIALIZER: str = "json"
+    CELERY_TASK_TIME_LIMIT: int = 600
+    CELERY_TASK_ACKS_LATE: bool = True
+    CELERY_WORKER_CONCURRENCY: int = 2
 
     class Config:
         env_file = ".env"
