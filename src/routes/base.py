0fcc5bfbf97acfcb33a732000479869ce77bb8f6
Gamal Osama <gamal5osama66@gmail.com>
2026-06-18 14:18:57 +0300

    adding a test task to the mail service, and a route to trigger it. This is for demonstration purposes only, and should be removed in production.

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,8 +1,14 @@
 from helpers.config import get_settings, Settings
+from tasks.mail_service import send_report
 
 from fastapi import FastAPI, APIRouter, Depends
 
 from datetime import datetime
+from time import sleep
+import logging
+
+
+logger = logging.getLogger("uvicorn.error")
 
 
 base_router = APIRouter(
@@ -21,3 +27,16 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
         "app_version": app_version,
         "date_&_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
+
+
+
+@base_router.get("/send_reports")
+async def send_reports(app_settings: Settings = Depends(get_settings)):
+    
+    task = send_report.delay(mail_wait_seconds=3)
+
+    return {
+        "success": "True",
+        "task_id": task.id
+    }
+    
