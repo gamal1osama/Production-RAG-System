a65b268ff985574a1a5d2c38af9116e4f4d2dbaa
Gamal Osama <gamal5osama66@gmail.com>
2026-06-20 22:58:41 +0300

    add the database to achieve the idempotent task

--- src/models/db_schemas/ragsys/schemas/__init__.py
+++ src/models/db_schemas/ragsys/schemas/__init__.py
@@ -1,5 +1,6 @@
 from .asset import Asset
 from .project import Project
 from .data_chunk import DataChunk, RetrievedDataChunk
+from .celery_task_execution import CeleryTaskExecution
 
 from .ragsys_base import SQLAlchemyBase
